59a931c65b4c7ff2e1b7bd0d9967c362f2d25a82
Maaz Siddique <siddiqma98@gmail.com>
2020-06-20 21:28:36 -0600

    Adding enter City feature

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
-var city ="Edmonton"
+$(document).ready(function(){
+    $('#button').click(function(){
+        var city =$('#txt1').val();
 
-$.getJSON("http://api.openweathermap.org/data/2.5/weather?q="+city+"&units=metric&appid=bc907e8c991c9d5928791235eef1324f",
- function(data){
-    console.log(data);
+        $.getJSON("http://api.openweathermap.org/data/2.5/weather?q="+city+"&units=metric&appid=bc907e8c991c9d5928791235eef1324f",
+        function(data){
+            console.log(data);
 
-    var displayPic = "http://openweathermap.org/img/w/" + data.weather[0].icon +".png";
-    var temperature = Math.floor(data.main.temp);
-    var weatherName = data.weather[0].main;
+            var displayPic = "http://openweathermap.org/img/w/" + data.weather[0].icon +".png";
+            var temperature = Math.floor(data.main.temp);
+            var weatherName = data.weather[0].main;
 
-    $('.weatherImg').attr('src', displayPic);
-    $('.weatherTemp').append(temperature)
-    $('.weatherName').append(weatherName)
+            $('.weatherImg').attr('src', displayPic);
+            $('.weatherTemp').text(temperature)
+            $('.weatherName').text(weatherName)
+        });
+    });
 });
+
